ed56152bd257ccf7b492693342c40b72312ebd01
RatovosonPascal <lratovoson@gmail.com>
2024-05-04 16:57:35 +0200

    first commit

---
+++ src/main/java/com/khal/studentpayment/StudentPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.khal.studentpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khal/studentpayment/StudentPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khal.studentpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
